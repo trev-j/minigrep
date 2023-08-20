3d76ca2e4d170576e1ec37f2db7dbc48c7466c0a
Trevor Hunter <trevor.j.hunter@outlook.com>
2023-08-20 10:17:42 -0600

    Added functionality to read contents of specified .txt file and print to console.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
+use std::fs;
 
 fn main() {
 
+    // Read in and store command line arguments
     let args: Vec<String> = env::args().collect();
     
     let query = &args[1];
@@ -9,4 +11,10 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+
+    // Read file contents
+    let contents = fs::read_to_string(file_path)
+        .expect("Failed to read file");
+    
+    println!("With text:\n{contents}");
 }
